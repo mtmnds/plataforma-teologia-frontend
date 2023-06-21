1ed5d729807736004e26f83f40468aba7073a65d
Matheus Mendes <mt.mendes@outlook.com>
2023-06-21 20:11:28 -0300

    Busca da lista de materiais com paginação.

--- src/app/pages/material-list-page/material-list-page.component.ts
+++ src/app/pages/material-list-page/material-list-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MaterialListPageService } from './material-list-page.service';
 
 @Component({
   selector: 'app-material-list-page',
@@ -7,43 +8,30 @@ import { Component } from '@angular/core';
 })
 export class MaterialListPageComponent {
 
-  public recentPubs: any[] = [
-    {
-      title: "Gênesis",
-      desc: "Esse é o livro das origens, pois conta a origem do mundo, da humanidade e do povo hebreu. Os prim...",
-      link: "material/genesis",
-      imgUrl: "assets/images/posts/genesis.jpg"
-    },
-    {
-      title: "Êxodo",
-      desc: "Êxodo inicia-se com a história do nascimento de Moisés, passa pela sua fuga do Egito e formação d...",
-      link: "material/exodo",
-      imgUrl: "assets/images/posts/exodo.jpg"
-    },
-    {
-      title: "Levítico",
-      desc: "Levítico é o livro da pureza, pois ressalta a santidade e pureza que o povo de Deus deveria ter, ...",
-      link: "material/levitico",
-      imgUrl: "assets/images/posts/levitico.png"
-    },
-    {
-      title: "Números",
-      desc: "Números continua a jornada dos hebreus rumo a terra prometida. O livro recebe esse nome por começ...",
-      link: "material/numeros",
-      imgUrl: "assets/images/posts/numeros.png"
-    },
-    {
-      title: "Deuteronômio",
-      desc: "Esse é o livro da repetição da Lei, pois muito tempo se passou desde que Deus deu a Lei em Êxodo....",
-      link: "material/deuteronomio",
-      imgUrl: "assets/images/posts/deuteronomio.png"
-    },
-    {
-      title: "Josué",
-      desc: "Josué é um livro de conteúdo militar, pois narra a conquista da terra prometida, Canaã, em todo o...",
-      link: "material/josue",
-      imgUrl: "assets/images/posts/josue.jpg"
-    },
-  ];
+  public recentPubs: any[] = [];
+
+  constructor(
+    private materialListPageService: MaterialListPageService
+  ) { }
+
+  ngOnInit(): void {
+    this.materialListPageService.buscarMateriaisPagina(1, 10).subscribe(
+      (res: any) => {
+        if (res) {
+          this.recentPubs = res.map((item: any) => {
+            return {
+              title: item.titulo,
+              desc: item.descricao,
+              link: `material/${item.id}`,
+              imgUrl: item.urlImagem
+            }
+          });
+        }
+      },
+      (err: any) => {
+        console.log(err);
+      }
+    );
+  }
 
 }
--- src/app/pages/material-list-page/material-list-page.service.ts
+++ src/app/pages/material-list-page/material-list-page.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MaterialListPageService {
 
-  constructor() { }
+  private urlBase = `${ environment.urlBase }`;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public buscarMateriaisPagina(numPagina: number, qtdRegistros: number) {
+    const url = `${this.urlBase}/material?numPagina=${numPagina}&qtdRegistros=${qtdRegistros}`;
+    return this.http.get(url);
+  }
 }
